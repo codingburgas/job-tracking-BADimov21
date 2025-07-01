fad28f14c47fd4d1a6a6a3c8f52655e64a0a8788
Bozhidar Dimov <BADimov21@codingburgas.bg>
2025-07-01 19:42:08 +0300

    Fix footer links to job ads and job applications pages

--- WEB/src/app/components/navigation/footer/footer.component.ts
+++ WEB/src/app/components/navigation/footer/footer.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
+  imports: [RouterModule],
+  standalone: true,
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
